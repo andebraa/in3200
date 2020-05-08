755ff5caaaec5d16eaac815e696b68d61f748c0f
anders bråte <andebraa@uio.no>
2020-05-08 17:53:13 +0200

    added updates to MPI functionality

--- IN3200_HE2_15120/mpi_count_friends_of_ten.c
+++ IN3200_HE2_15120/mpi_count_friends_of_ten.c
@@ -2,39 +2,81 @@
 #include <stdio.h>
 #include <math.h>
 #include <mpi.h>
-#include "omp.h"
 #include "string.h"
 
 
-int MPI_count_friends_of_ten(int M, int N, int ** v){
-  printf("inside count friends \n");
+int MPI_count_friends_of_ten(int M, int N, int** v){
   int friends = 0;
   int comm_sz;
   int my_rank;
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  printf("comm %d\n", comm_sz);
 
-  /*
-  if(M%comm_sz != 0){
-
-  }
-  for(int i = 0; i < comm_sz; i++){
-
-  }
-  */
-//  if(my_rank==1){
-    for(int i = 0; i < M; i++){
-      for(int j = 0; j < N; j++){
-        printf("  %d  ", v[i][j]);
-      }
-      printf("\n");
+  int * rows = (int*)calloc(comm_sz+1, sizeof(int));
+  if(my_rank==0){
+    for(int i = 0; i < (comm_sz); i++){
+      rows[i +1] = rows[i] + (M/comm_sz-1); //stores cumulative number of rows
     }
-    printf("end friend loop \n");
-//  }else if(my_rank == comm_sz){ //last block does can not include ghost points
+    //note! whenever using comm_sz, you must subtract 1 to get index
+    rows[comm_sz] = M-1; //last element is given the rest
+    //this might lead to load imbalance if rest is very large
+    MPI_Bcast(rows, comm_sz+1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(N, 1, MPI_INT, 0, MPI_COMM_WORLD); //Sending size of rows, since all threads
+    //will recive full rows
 
-  //}else{ //all blocks and ranks inbetween first and last
+    //sending rows to other threads EXCEPT last thread as it does not want ghost rows
+    for(int i = 1; i < comm_sz-1; i ++){//loop over threads except last thread
+      for(int r = rows[i]+1; r < rows[i+1]+2; r ++){//loops over number of rows for each thread
+        //in order to calculate vertical friends it needs two ghost rows
+        MPI_Send(v[r], N, MPI_INT, i, 0, MPI_COMM_WORLD);
+        // data, count, data type, destination, tag, communicator
+        // i is the thread which data is sent to
+      }
+    }
+    //the last thread is given the bottom rows, hence it can not be given
+    //and ghost rows! comm_sz -1 is the last thread, who's index is comm_sz
+    for(int r = rows[comm_sz-1]; r < rows[comm_sz]; r ++){
+      MPI_send(v[r], N, MPI_INT, comm_sz-1, 0, MPI_COMM_WORLD);
+    }
 
-  //}
+
+    CALCULATE RANK 0 FRIENDS HERE
+
+  }else if(my_rank == (comm_sz-1)){ //last block does can not include ghost points
+  int n;
+  int * rows = (int*)calloc(comm_sz+2, sizeof(int));
+  MPI_Bcast(rows, comm_sz+1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  //callocating memory for the rows this thread will recieve
+  v = (int**)calloc(rows[comm_sz]-rows[comm_sz-1], sizeof(int*));
+  for(int i = 0; i < M; i++){
+    (v)[i] = (int*)calloc(N, sizeof(int));
+  }
+
+  }else{ //all blocks and ranks inbetween first and last
+  int n;
+  int * rows = (int*)calloc(comm_sz+2, sizeof(int));
+  MPI_Bcast(rows, comm_sz+1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  }
+
+//the middle threads are given space for two ghost rows.
+  v = (int**)calloc( (rows[my_rank+1] -rows[my_rank]) +2, sizeof(int*)); //making 2D array
+  for(int i = 0; i < M; i++){
+    (v)[i] = (int*)calloc(N, sizeof(int));
+    for(int j = 0; j < N; j++){
+  }
+
+  
+  for(int i = 1; i < comm_sz-1; i ++){//loop over threads except last thread
+    for(int r = rows[i]+1; r < rows[i+1]+2; r ++){//loops over number of rows for each thread
+      //in order to calculate vertical friends it needs two ghost rows
+      MPI_Send(v[r], N, MPI_INT, i, 0, MPI_COMM_WORLD);
+      // data, count, data type, destination, tag, communicator
+      // i is the thread which data is sent to
+    }
+  }
 
   return friends;
 }
--- IN3200_HE2_15120/mpi_main.c
+++ IN3200_HE2_15120/mpi_main.c
@@ -12,33 +12,30 @@ int main (int argc, char **argv){
   MPI_Init (&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   if (rank==0) {
-    int M = 7;
-    int N = 10;
+    M = 9;
+    N = 9;
     v = (int**)calloc(M, sizeof(int*)); //making 2D array
     for(int i = 0; i < M; i++){
       (v)[i] = (int*)calloc(N, sizeof(int));
       for(int j = 0; j < N; j++){
-        v[i][j] = 1 + rand()%N; //note, [] derefrences the variable once
+        v[i][j] = 1 + rand()%9; //note, [] derefrences the variable once
       }
     }
-    /*
-    for(int i = 0; i < M; i++){
-      for(int j = 0; j < N; j++){
-        printf("  %d  ", v[i][j]);
-      }
-      printf("\n");
-    }
-    printf("end main loop \n");
-    */
+
   } //end rank = 0
-  num_triple_friends = MPI_count_friends_of_ten (M, N, v);
+
+  num_triple_friends =MPI_count_friends_of_ten (M, N, v);
+
   printf("MPI rank <%d>: number of triple friends=%d\n", rank, num_triple_friends);
+
+/*
   if (rank==0){
     for(int i = 0; i < M; i++){
       free(v[i]);
     }
   free(v);
   }
+  */
   MPI_Finalize ();
   return 0;
 }
